ba29272a8db5521b4410d080e89f29094a5f8393
MihailV30 <mihailv3011@gmail.com>
2025-01-26 04:54:53 +0700

    initial commit

--- этап 2/update_note_function.py
+++ этап 2/update_note_function.py
@@ -1,26 +1,35 @@
 from datetime import datetime
 
 def validate_date(date_str): #Проверяет корректность формата даты.
-
     try:
         datetime.strptime(date_str, '%d-%m-%Y')
         return True
-    except:
+    except ValueError:
         return False
 
-def update_note(notes_list):    #Обновляет заметку, позволяя пользователю выбрать поле для обновления.
-
+def update_note(notes_list): #Обновляет заметку, позволяя пользователю выбрать поле для обновления.
     if not notes_list:
-        print("Список заметок пуст")
+        print("Список заметок пуст.")
         return notes_list
 
-    print("Текущие данные заметки:")
-    print(notes_list)
+    print("Текущие заметки:")
+    for i, note in enumerate(notes_list):
+        print(f"{i+1}. {note}")
+
+    while True: #выбор номера заметки для обновления
+        try:
+            note_index = int(input("Введите номер заметки для обновления: ")) - 1
+            if 0 <= note_index < len(notes_list):
+                break
+            else:
+                print("Неверный номер заметки.")
+        except ValueError:
+            print("Некорректный ввод. Пожалуйста, введите число.")
 
     updatable_fields = ["username", "title", "content", "status", "issue_date"]
     while True: #цикл работает пока пользователь не введет кореектное имя поля для обновления
-        field_to_update = input(f"Какие данные вы хотите обновить? ({', '.join(updatable_fields)}): ")
-        if field_to_update in updatable_fields:
+        field_to_update = input(f"Какие данные вы хотите обновить? ({', '.join(updatable_fields)}): ").lower()
+        if field_to_update in [f.lower() for f in updatable_fields]:
             break
         else:
             print("Некорректное имя поля. Пожалуйста, попробуйте еще раз.")
@@ -30,15 +39,15 @@ def update_note(notes_list):    #Обновляет заметку, позвол
         if field_to_update == "issue_date" and not validate_date(new_value):
             print("Некорректный формат даты. Используйте формат ДД-ММ-ГГГГ.")
         else:
-            notes_list[field_to_update] = new_value
+            notes_list[note_index][field_to_update] = new_value
             break
 
     print("\nЗаметка обновлена:")
-    print(notes_list)
+    print(notes_list[note_index])
     return notes_list
 
-
 # Пример использования
+'''
 note = {
     'username': 'Алексей',
     'title': 'Список покупок',
@@ -47,5 +56,6 @@ note = {
     'created_date': '27-11-2024',
     'issue_date': '30-11-2024'
 }
-
+'''
 #updated_note = update_note(note) #вызов закоментирован для корректной работы меню при импорте файла с ней
+
